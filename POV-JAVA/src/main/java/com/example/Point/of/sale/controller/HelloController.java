6be24dfef07b0b44d4ad1aeda18fd1bc4fd9fbc4
HansLanda14ib <bibzazne@gmail.com>
2024-01-07 21:06:16 +0100

    v4-webhook

--- POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ POV-JAVA/src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String hello(){
-        return "Hello V3 from spring boot application :)";
+        return "Hello WEBHOOK case from spring boot application :)";
     }
 
     @GetMapping("/user")
